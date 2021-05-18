40fe21fbdd97d93c36c2276ba27dc5096819fff1
John Pranoy Yalla <jyalla@evoketechnologies.com>
2021-05-18 20:06:58 +0530

    Renamed Project

--- user-queue-listener/src/main/java/com/jyalla/reciever/UserQueueListenerApplication.java
+++ user-queue-listener/src/main/java/com/jyalla/reciever/UserQueueListenerApplication.java
@@ -6,13 +6,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeMQRecieverApplication {
+public class UserQueueListenerApplication {
 
-    private static Logger logger = LoggerFactory.getLogger(EmployeeMQRecieverApplication.class);
+    private static Logger logger = LoggerFactory.getLogger(UserQueueListenerApplication.class);
 
     public static void main(String[] args) {
-        logger.info("EmployeeMQRecieverApplication Started");
-        SpringApplication.run(EmployeeMQRecieverApplication.class, args);
+        logger.info("UserQueueListenerApplication Started");
+        SpringApplication.run(UserQueueListenerApplication.class, args);
     }
 
     // @ResponseStatus(HttpStatus.BAD_REQUEST)
